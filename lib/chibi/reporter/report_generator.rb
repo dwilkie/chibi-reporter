a98629eb7004c0fd471321fedb35cc8b5729505a
David Wilkie <dwilkie@gmail.com>
2013-12-12 15:21:11 +0700

    add logic to prevent operator reports from being emailed more than once

--- lib/chibi/reporter/report_generator.rb
+++ lib/chibi/reporter/report_generator.rb
@@ -8,21 +8,23 @@ module Chibi
 
       Dir[File.join(File.dirname(__FILE__), '**/*.rb')].each { |file| require file }
 
+      REPORTS_KEY = "reports"
+      OPERATOR_KEY = "operator"
       LAST_INVOICE_NUMBER_KEY = "last_invoice_number"
 
       attr_accessor :invoice_number
 
       def run!
         return unless remote_report
+        self.invoice_number = reports_operator_metadata[LAST_INVOICE_NUMBER_KEY].to_i
 
-        self.invoice_number = metadata[LAST_INVOICE_NUMBER_KEY].to_i
         with_operator_reports do |operator_report|
           operator_report.generate!
           distribute(operator_report)
           self.invoice_number += 1
         end
 
-        write_invoice_number if increment_invoice_number?
+        write_reports_operator_metadata unless operator_reports_exist?
       end
 
       private
@@ -30,7 +32,7 @@ module Chibi
       def distribute(operator_report)
         upload_to_s3(operator_report)
         upload_to_google_drive(operator_report)
-        email(operator_report) if operator_report.email_enabled?
+        email(operator_report) if !operator_reports_exist? && operator_report.email_enabled?
       end
 
       def upload_to_s3(operator_report)
@@ -100,8 +102,20 @@ module Chibi
         report_data["year"]
       end
 
+      def operator_reports_exist?
+        (reports_operator_metadata[year.to_s] || {})[month.to_s]
+      end
+
       def metadata
         @metadata ||= JSON.parse(metadata_file.read)
+      end
+
+      def reports_metadata
+        metadata[REPORTS_KEY] || {}
+      end
+
+      def reports_operator_metadata
+        reports_metadata[OPERATOR_KEY] || {}
       end
 
       def metadata_file
@@ -128,12 +142,17 @@ module Chibi
         @report_mailer ||= ReportMailer.new
       end
 
-      def increment_invoice_number?
-        ENV["CHIBI_REPORTER_REPORT_GENERATOR_INCREMENT_INVOICE_NUMBER"].to_i == 1
-      end
-
-      def write_invoice_number
-        metadata_file.write(metadata.merge(LAST_INVOICE_NUMBER_KEY => invoice_number).to_json)
+      def write_reports_operator_metadata
+        metadata_file.write(
+          metadata.deep_merge(
+            REPORTS_KEY => {
+              OPERATOR_KEY => {
+                LAST_INVOICE_NUMBER_KEY => invoice_number,
+                year => {month => Time.current}
+              }
+            }
+          ).to_json
+        )
       end
     end
   end
--- spec/chibi/reporter/report_generator_spec.rb
+++ spec/chibi/reporter/report_generator_spec.rb
@@ -30,12 +30,19 @@ module Chibi
 
       describe "#run!" do
         let(:last_invoice_number) { 40 }
-        def expect_report_generator_run!(&block)
+        def expect_report_generator_run!(options = {}, &block)
+          metadata = {
+            "reports" => {
+              "operator" => {
+                "last_invoice_number" => last_invoice_number
+              }.merge(options[:reports_operator_metadata] || {})
+            }
+          }.deep_merge(options[:metadata] || {})
           expect_chibi_client_get_remote_report(
             :report_generator_run,
             :erb => {
               :aws_s3_metadata_url => aws_s3_metadata_url,
-              :metadata => {:last_invoice_number => last_invoice_number}
+              :metadata => metadata#.to_json
             }.merge(
               google_drive_upload_erb(:files => expected_files(:google_drive))
             ).merge(
@@ -46,35 +53,61 @@ module Chibi
         end
 
         context "given the remote report is available" do
-          before do
-            expect_report_generator_run! { subject.run! }
+          def expect_report_generator_run!(options = {})
+            super(options) { subject.run! }
           end
 
-          it "should increment the invoice number" do
-            num_operator_reports = asserted_operators.inject(0) {|total, (k, v)| total + v.size}
-            subject.invoice_number.should == num_operator_reports + last_invoice_number
+          context "but operator reports have already been generated for this month and year" do
+            def expect_report_generator_run!
+              super(:reports_operator_metadata => {report_year => {report_month => 2.days.ago}})
+            end
+
+            it "should not update any metadata" do
+              expect_report_generator_run!
+              last_request.uri.should_not == aws_s3_metadata_url
+            end
+
+            it "should not send out any emails" do
+              expect_report_generator_run!
+              mail_deliveries.should be_empty
+            end
           end
 
-          it "should send an email for each operator report" do
-            with_asserted_operators(:email_enabled => true) do |country_code, operator_id, index|
-              mail_delivery = mail_deliveries[index]
-              mail_delivery.from.should == [mail_sender(country_code, operator_id)]
-              mail_delivery.to.should == mail_recipients(country_code, operator_id)
-              mail_delivery.cc.should == mail_cc(country_code, operator_id)
-              mail_delivery.bcc.should == mail_bcc(country_code, operator_id)
-              mail_delivery.subject.should == mail_subject(
-                report_year, report_month, country_code, operator_id
-              )
-              mail_delivery.text_part.decoded.should == mail_body(
-                report_year, report_month, country_code, operator_id
-              )
-              attachment = mail_delivery.attachments.first
-              attachment.filename.should == File.basename(
-                operator_suggested_filename(
+          context "and no operator reports have been generated for this month and year" do
+            it "should increment the metadata for the last invoice number" do
+              Timecop.freeze(Time.current) do
+                expect_report_generator_run!
+                num_operator_reports = asserted_operators.inject(0) {|total, (k, v)| total + v.size}
+                asserted_last_invoice_number = num_operator_reports + last_invoice_number
+                last_request(:url).should == aws_s3_metadata_url
+                metadata_update = JSON.parse(last_request.body)["reports"]["operator"]
+                metadata_update["last_invoice_number"].should == asserted_last_invoice_number
+                metadata_update[report_year.to_s][report_month.to_s].should == Time.current.to_s
+              end
+            end
+
+            it "should send an email for each operator report" do
+              expect_report_generator_run!
+              with_asserted_operators(:email_enabled => true) do |country_code, operator_id, index|
+                mail_delivery = mail_deliveries[index]
+                mail_delivery.from.should == [mail_sender(country_code, operator_id)]
+                mail_delivery.to.should == mail_recipients(country_code, operator_id)
+                mail_delivery.cc.should == mail_cc(country_code, operator_id)
+                mail_delivery.bcc.should == mail_bcc(country_code, operator_id)
+                mail_delivery.subject.should == mail_subject(
                   report_year, report_month, country_code, operator_id
                 )
-              )
-              attachment.content_type.split(";").first.should == mime_type
+                mail_delivery.text_part.decoded.should == mail_body(
+                  report_year, report_month, country_code, operator_id
+                )
+                attachment = mail_delivery.attachments.first
+                attachment.filename.should == File.basename(
+                  operator_suggested_filename(
+                    report_year, report_month, country_code, operator_id
+                  )
+                )
+                attachment.content_type.split(";").first.should == mime_type
+              end
             end
           end
         end
