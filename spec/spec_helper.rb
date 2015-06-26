bafdd897e79e2e2495911d259f1ddfa00e8c7dc4
David Wilkie <dwilkie@gmail.com>
2015-06-26 20:45:42 +0700

    Use new RSpec syntax. Closes #14

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
 RSpec.configure do |config|
   Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
-
-  config.expect_with :rspec do |c|
-    c.syntax = [:should, :expect]
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.syntax = [:should, :expect]
-  end
 end
